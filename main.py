98dbdac12b42ca65cfe67feb6c3112af4243d71d
Dee <playerd@mijumaru.net>
2023-05-06 14:03:11 +0300

    fixes

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import e621py_wrapper as e621
 
 
-def download(ident, cat):
-    print(f'Downloading post {ident} in category {cat}...')
-    better_cat = cat.replace('/', ' slash ')
-    better_cat = better_cat.replace('_', ' ')
-    api.util.save(ident, f"{better_cat}/")
+def download(ident, cat, par):
+    if par:
+        print(f'Downloading post {ident}, the child of {cat}')
+    else:
+        print(f'Downloading post {ident} in category {cat}...')
+        cat = cat.replace('/', ' slash ')
+        cat = cat.replace('_', ' ')
+    api.util.save(ident, f"{cat}/")
     print('Done')
 
 
 def uncategorized_download(ident):
     print(f'Downloading post {ident}, uncategorized...')
     api.util.save(ident, "uncategorized/")
-    print('done')
+    print('Done')
 
 
 def download_pool(pool_id):
@@ -20,7 +23,7 @@ def download_pool(pool_id):
     for ident in pool[0]['post_ids']:
         print(f'Downloading post {ident} from pool {pool[0]["name"]}...')
         api.util.save(ident, f"{pool[0]['name'].replace('/', ' ')}")
-        print('done')
+        print('Done')
     print(f'Done downloading pool {pool[0]["name"]}')
 
 
@@ -38,16 +41,33 @@ def main():
                 break
             download_pool(pool_id)
             pool_downloaded_list.append(pool_id)
-        if not pool_download:
-            match = None
-        for tag in all_tags:
-            for category in categories:
-                if tag == category:
-                    match = category
-        if match is not None:
-            download(post['id'], match)
-        else:
-            uncategorized_download(post['id'])
+        if post['relationships']['parent_id'] is not None or not post['relationships']['has_children']:
+            if not pool_download:
+                match = None
+                for tag in all_tags:
+                    for category in categories:
+                        if tag == category:
+                            match = category
+                if match is not None:
+                    download(post['id'], match, False)
+                else:
+                    uncategorized_download(post['id'])
+        elif post['relationships']['has_children']:
+            if post['relationships']['parent_id'] is None:
+                download(post['id'], post['id'], True)
+                for child in post['relationships']['children']:
+                    new_child = api.posts.get(child)
+                    if new_child['relationships']['has_children']:
+                        for wow in new_child['relationships']['children']:
+                            newer_child = api.posts.get(wow)
+                            while newer_child['relationships']['has_children']:
+                                for why in newer_child['relationships']['children']:
+                                    i_hate_myself = api.posts.get(why)
+                                    download(i_hate_myself['id'], post['id'], True)
+                                    posts.remove(newer_child)
+                                    newer_child = api.posts.get(i_hate_myself['relationships']['children'][1])
+                    download(new_child['id'], post['id'], True)
+                    posts.remove(new_child)
 
 
 api = e621.client()
@@ -65,7 +85,6 @@ while not rate_set:
     except ValueError:
         rate_set = False
 for post_rate in posts:
-    # noinspection PyUnboundLocalVariable
     if post_rate['score']['total'] < rate:
         posts.remove(post_rate)
 main()    # leftover!
